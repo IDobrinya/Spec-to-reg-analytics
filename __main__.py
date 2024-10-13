7211e6d1ad7113dbc20985b3ae281be51385e5b8
Dobrinya <mitin1005@gmail.com>
2024-10-13 07:49:37 +0300

    develop

--- __main__.py
+++ __main__.py
@@ -1,59 +1,70 @@
+import nltk
 import streamlit as st
 import streamlit_authenticator as stauth
 from streamlit_option_menu import option_menu
 
 from pages_ import logins
+from pages_ import mistakes
 from services.logins_service import LoginsService
 
 
+nltk.download('stopwords')
+nltk.download('wordnet')
+
+
 def main():
-    st.set_page_config(page_title="ANALYTICS", layout="wide", page_icon="🔥")
+    st.set_page_config(page_title="ATOM", layout="wide", page_icon="⚡")
 
-    if "credentials" not in st.session_state:
-        logins_service = LoginsService()
-        st.session_state.credentials = logins_service.credentials()
+    # if "credentials" not in st.session_state:
+    #     logins_service = LoginsService()
+    #     st.session_state.credentials = logins_service.credentials()
+    #
+    # authenticator = stauth.Authenticate(
+    #     credentials=st.session_state.credentials,
+    #     cookie_name="cookies_name",
+    #     cookie_key="cookies_key",
+    #     cookie_expiry_days=30.0
+    # )
+    #
+    # name, state, username = authenticator.login()
+    #
+    # if state:
+    #     # if "rerun" not in st.session_state:
+    #     #     st.session_state.rerun = True
+    #     #     st.rerun()
+    #
+    #     name = st.session_state.credentials['usernames'][username]['name']
+    name = "admin"
 
-    authenticator = stauth.Authenticate(
-        credentials=st.session_state.credentials,
-        cookie_name="cookies_name",
-        cookie_key="cookies_key",
-        cookie_expiry_days=30.0
-    )
+    with st.sidebar:
 
-    name, state, username = authenticator.login()
+        options = [
+            "Reasons",
+            "Logins"
+        ]
+        icons = [
+            "bi bi-activity",
+            "bi bi-key"
+        ]
 
-    if state:
-        if "rerun" not in st.session_state:
-            st.session_state.rerun = True
-            st.rerun()
+        selected = option_menu(
+            menu_title="Analytics",
+            options=options,
+            icons=icons
+        )
 
-        name = st.session_state.credentials['usernames'][username]['name']
+        st.subheader(f"Welcome, :violet[{name.capitalize()}]\n")
 
-        with st.sidebar:
+    if selected == "Reasons":
+        mistakes.app()
+    elif selected == "Logins":
+        logins.app()
 
-            options = [
-                "Logins"
-            ]
-            icons = [
-                "bi bi-key"
-            ]
-
-            selected = option_menu(
-                menu_title="Navigation",
-                options=options,
-                icons=icons
-            )
-
-            st.subheader(f"Welcome, :violet[{name.capitalize()}]\n")
-
-        if selected == "Logins":
-            logins.app()
-
-    elif st.session_state["authentication_status"] is False:
-        st.error('Username/password is incorrect')
-
-    elif st.session_state["authentication_status"] is None:
-        st.warning('Please enter your username and password')
+    # elif st.session_state["authentication_status"] is False:
+    #     st.error('Username/password is incorrect')
+    #
+    # elif st.session_state["authentication_status"] is None:
+    #     st.warning('Please enter your username and password')
 
 
 if __name__ == "__main__":
